f5149050f6a003604be4477173717ff9b5da1750
DELL <DELL@DESKTOP-AI5KK88>
2026-06-02 00:34:58 +0300

    Add project files.

---
+++ P4- Currency Exchange.cpp
@@ -0,0 +1,17 @@
+// P4- Currency Exchange.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+
+{
+
+    while (true)
+    {
+        clsLoginScreen::ShowLoginScreen();
+    }
+
+    return 0;
+}
